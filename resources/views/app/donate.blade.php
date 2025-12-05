5305cb7993aa84d58e8846125b5f2be74849b918
salsabila <salsabilachandraa@gmail.com>
2025-12-05 21:43:02 +0700

    tampilan form donasi

---
+++ resources/views/app/donate.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.app')
+@section('title', 'Donate')
+
+@section('content')
+    <div class="h-100 gap-5 row justify-content-center text-black py-5">
+        <h1 class="text-center">Donate Now</h1>
+        <div class="row col-12 justify-content-center align-items-center gap-5">
+            <div class="card" style="width: 18rem;">
+                <a href="/donate-detail" class="text-decoration-none text-black">
+                    <img src="{{ asset('assets/landing_page.jpg') }}" class="card-img-top" alt="...">
+                    <div class="mb-2">
+                        <h5 class="card-title">Pray for Medan</h5>
+                        <p class="card-text">Donasi untuk saudara kita yang terkena bencana alam di Medan Sumatera Barat.
+                        </p>
+                    </div>
+                    <div class="d-flex row">
+                        <span class="py-2 border-top fw-semibold">081234567891</span>
+                        <span class="py-2 border-top fw-semibold">BNI a.n Ivan Gunawan (123456789)</span>
+                        <span class="py-2 border-top fw-semibold">Rp. 1.000.000</span>
+                    </div>
+                </a>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <a href="/donate-detail" class="text-decoration-none text-black">
+                    <img src="{{ asset('assets/landing_page.jpg') }}" class="card-img-top" alt="...">
+                    <div class="mb-2">
+                        <h5 class="card-title">Pray for Medan</h5>
+                        <p class="card-text">Donasi untuk saudara kita yang terkena bencana alam di Medan Sumatera Barat.
+                        </p>
+                    </div>
+                    <div class="d-flex row">
+                        <span class="py-2 border-top fw-semibold">081234567891</span>
+                        <span class="py-2 border-top fw-semibold">BNI a.n Ivan Gunawan (123456789)</span>
+                        <span class="py-2 border-top fw-semibold">Rp. 1.000.000</span>
+                    </div>
+                </a>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <a href="/donate-detail" class="text-decoration-none text-black">
+                    <img src="{{ asset('assets/landing_page.jpg') }}" class="card-img-top" alt="...">
+                    <div class="mb-2">
+                        <h5 class="card-title">Pray for Medan</h5>
+                        <p class="card-text">Donasi untuk saudara kita yang terkena bencana alam di Medan Sumatera Barat.
+                        </p>
+                    </div>
+                    <div class="d-flex row">
+                        <span class="py-2 border-top fw-semibold">081234567891</span>
+                        <span class="py-2 border-top fw-semibold">BNI a.n Ivan Gunawan (123456789)</span>
+                        <span class="py-2 border-top fw-semibold">Rp. 1.000.000</span>
+                    </div>
+                </a>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <a href="/donate-detail" class="text-decoration-none text-black">
+                    <img src="{{ asset('assets/landing_page.jpg') }}" class="card-img-top" alt="...">
+                    <div class="mb-2">
+                        <h5 class="card-title">Pray for Medan</h5>
+                        <p class="card-text">Donasi untuk saudara kita yang terkena bencana alam di Medan Sumatera Barat.
+                        </p>
+                    </div>
+                    <div class="d-flex row">
+                        <span class="py-2 border-top fw-semibold">081234567891</span>
+                        <span class="py-2 border-top fw-semibold">BNI a.n Ivan Gunawan (123456789)</span>
+                        <span class="py-2 border-top fw-semibold">Rp. 1.000.000</span>
+                    </div>
+                </a>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <a href="/donate-detail" class="text-decoration-none text-black">
+                    <img src="{{ asset('assets/landing_page.jpg') }}" class="card-img-top" alt="...">
+                    <div class="mb-2">
+                        <h5 class="card-title">Pray for Medan</h5>
+                        <p class="card-text">Donasi untuk saudara kita yang terkena bencana alam di Medan Sumatera Barat.
+                        </p>
+                    </div>
+                    <div class="d-flex row">
+                        <span class="py-2 border-top fw-semibold">081234567891</span>
+                        <span class="py-2 border-top fw-semibold">BNI a.n Ivan Gunawan (123456789)</span>
+                        <span class="py-2 border-top fw-semibold">Rp. 1.000.000</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
